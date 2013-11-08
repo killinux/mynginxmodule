2001208fa8639713e59c4d0b2998b6d069d22920
root <root@VM_12_197_centos.(none)>
2013-11-09 00:49:25 +0800

    vim

--- nginx_release/nginx-1.5.6/make.sh
+++ nginx_release/nginx-1.5.6/make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf /usr/local/nginx_hello
-./configure --prefix=/usr/local/nginx_hello --add-module=/root/mymodule/mynginxmodule/mynginx/helloworld
+./configure --prefix=/usr/local/nginx_hello --add-module=/root/haoning/mygit/mynginxmodule/mynginx/helloworld
 make 
 make install
-cp -f /root/mymodule/mynginxmodule/mynginx/helloworld/nginx.conf /usr/local/nginx_hello/conf
+cp -f /root/haoning/mygit/mynginxmodule/mynginx/helloworld/nginx.conf /usr/local/nginx_hello/conf
