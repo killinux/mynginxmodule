ad7a5d7cdc5d92cf1e82b598f2ac85d0c77b3952
killinux <killinux@163.com>
2014-04-15 10:40:23 +0800

    delete no use file

--- nodejs/websocket/java/run.sh
+++ nodejs/websocket/java/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-nohup /usr/local/java/jdk1.6.0_45/bin/java -cp .:./WebSocket.jar ChatServer >>/data/haoning/mygit/mynginxmodule/nodejs/websocket/java/websocket.log  2>&1 &
+#nohup /usr/local/java/jdk1.6.0_45/bin/java -cp .:./WebSocket.jar ChatServer >>/data/haoning/mygit/mynginxmodule/nodejs/websocket/java/websocket.log  2>&1 &
+nohup /usr/local/java/jdk1.6.0_45/bin/java -cp .:./WebSocket.jar ChatServer >/dev/null  2>&1 &
